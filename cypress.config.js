56cd15bde812a0629669f191c47728ae01423b0f
J. Vitor Castro <jcastro@procenge.com.br>
2025-05-29 21:11:13 -0300

    Mocha Awesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,15 @@
-const { defineConfig } = require("cypress");
-const date = require('dayjs');
+const { defineConfig } = require("cypress")
+const date = require('dayjs')
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
     env: {
       date: date().format('DD/M/YYYY'),
+      allureAddVideoOnPass: true
     },
     viewportWidth: 1280,
     viewportHeight: 720,
