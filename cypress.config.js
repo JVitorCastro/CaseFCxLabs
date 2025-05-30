6047508c26afaa58630d0668df7fceefad225f98
J. Vitor Castro <jcastro@procenge.com.br>
2025-05-29 21:30:44 -0300

    Configurações para gravação dos vídeos.

--- cypress.config.js
+++ cypress.config.js
@@ -3,13 +3,13 @@ const date = require('dayjs')
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on)
     },
     env: {
-      date: date().format('DD/M/YYYY'),
-      allureAddVideoOnPass: true
+      date: date().format('DD/M/YYYY')
     },
     viewportWidth: 1280,
     viewportHeight: 720,
